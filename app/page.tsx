2035df4c0c10caf4e3953c91d41c366d147de94f
= <=>
2025-05-30 19:21:49 +0600

    Dialleads home page clone started

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <>
+      <section className="flex justify-center p-3">
+        <div className="flex w-full px-5 rounded-xl bg-black h-screen justify-center items-center">
+          <h2 className="text-6xl text-white text-center font-bold">
+            Effortless Human-Like <br />
+            <span className="bg-clip-text bg-transparent bg-gradient-to-r from-purple-800 to-purple-300 text-transparent">
+              AI Phone Calls
+            </span>
+          </h2>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
